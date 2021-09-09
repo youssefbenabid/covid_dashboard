a75fedeefab68c17fc669c2dfb697c4aa9dd1d9f
youssefbenabid <youssef.benabid1996@gmail.com>
2021-09-09 21:20:58 +0200

    covid_dashboard

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:covid_dashboard_ui/screens/bottom_nav_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:covid_dashboard_ui/screens/screens.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Covid-19 Dashboard UI',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: BottomNavScreen(),
+    );
+  }
+}
